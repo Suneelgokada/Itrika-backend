3bd59e5a8ee5e2310fb1435773853d87de493db6
suneel <suneelgokada@gmail.com>
2026-02-14 12:20:26 +0530

    comments removed

--- models/user.js
+++ models/user.js
@@ -9,14 +9,12 @@ const userSchema = new mongoose.Schema({
     isActive: { type: Boolean, default: true }
 }, { timestamps: true });
 
-// Password Save ayye mundu encrypt cheyadam
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) next();
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
 
-// Login appudu password check cheyadaniki helper
 userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
